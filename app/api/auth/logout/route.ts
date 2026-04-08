ebfe703c8ac36699477b791ebc0ec451f575c04b
bayuwijanarko07 <bayuwijanarko07@gmail.com>
2026-04-08 15:56:26 +0700

    feat: logout redirect login

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,10 +1,9 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(req: Request) {
   const cookieStore = await cookies();
-  
-  // Hapus cookie token
+
   cookieStore.set("token", "", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
@@ -13,5 +12,5 @@ export async function POST() {
     expires: new Date(0),
   });
 
-  return NextResponse.json({ message: "Logout berhasil" });
+  return NextResponse.redirect(new URL("/login", req.url), { status: 302 });
 }
